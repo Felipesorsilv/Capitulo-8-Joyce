528d6fef656f9a93f0765aca1749626ffc6b6cab
Felipesorsilv <61020704+Felipesorsilv@users.noreply.github.com>
2021-02-28 22:27:08 -0600

    Add files via upload

---
+++ Cap8_Joyce/src/Gamezone1/Inciso.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gamezone1;
+
+/**
+ *
+ * @author felipe
+ */
+public class Inciso {
+     private String respuesta;
+    private Boolean correcto;
+
+    public Inciso(String respuesta, Boolean correcto) {
+        this.respuesta = respuesta;
+        this.correcto = correcto;
+    }
+
+    public String getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+    public Boolean getCorrecto() {
+        return correcto;
+    } 
+}
